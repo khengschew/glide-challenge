a4493618fd01036e4995b60423f3ad248a0e1da9
khengschew <khengc@gmail.com>
2019-07-22 10:21:34 -0700

    Implement auth token retrieval

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,26 @@
+const axios = require("axios");
+const formurlencoded = require("form-urlencoded").default;
+
+const fetchToken = () => {
+  return axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: formurlencoded({
+      grant_type: "client_credentials",
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${process.env.CREDENTIALS}`,
+    },
+  });
+};
+
 module.exports.getToken = (req, res) => {
-  res.end("hello");
+  fetchToken()
+    .then((response) => {
+      res.end(JSON.stringify(response.data));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
