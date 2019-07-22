2a728b51bf19fe4fbe8287893a8e5b7882e6a7bb
khengschew <khengc@gmail.com>
2019-07-22 07:03:33 -0700

    Setup basic express server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../client/public")));
+
+app.listen(port, () => console.log(`listening on port: ${port}`));
