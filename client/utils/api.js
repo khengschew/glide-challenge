e0cb3ca6fc5b4575c264f93c852703e9814ff1b7
khengschew <khengc@gmail.com>
2019-07-22 15:20:48 -0700

    Refactor spotify query helper to separate query and url

--- client/utils/api.js
+++ client/utils/api.js
@@ -4,15 +4,17 @@ const Api = {
   searchSpotify: (q) => {
     q = encodeURIComponent(q);
 
+    return Api.spotifyRequest(
+      `https://api.spotify.com/v1/search?q=${q}&type=artist,track`
+    );
+  },
+  spotifyRequest: (url) => {
     return Auth.getToken().then((token) => {
-      return fetch(
-        `https://api.spotify.com/v1/search?q=${q}&type=artist,track`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      ).then((response) => response.json());
+      return fetch(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((response) => response.json());
     });
   },
 };
