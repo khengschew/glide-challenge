a9b065e96edc5d71e1e81535915d0f9799a5acb5
khengschew <khengc@gmail.com>
2019-07-22 19:22:26 -0700

    Refactor app and search components to set state from query

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { Input } from "antd";
-import Api from "../utils/api";
 import "antd/dist/antd.css";
 
 const SearchField = Input.Search;
 
-const Search = (props) => (
+const Search = ({ onSearch }) => (
   <div id="search">
     <SearchField
       placeholder="Search for artist or track"
-      onSearch={(value) =>
-        Api.searchSpotify(value).then((response) => console.log(response))
-      }
+      onSearch={onSearch}
       size="large"
       id="search-field"
       style={{
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import Search from "./Search";
+import ArtistList from "./ArtistList";
+import TrackList from "./TrackList";
+import Api from "../utils/api";
 
-const App = (props) => (
-  <>
-    <Search />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      artists: [],
+      tracks: [],
+    };
+
+    this.onSearch = this.onSearch.bind(this);
+  }
+
+  onSearch(query) {
+    Api.searchSpotify(query).then((response) => {
+      this.setState(response);
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Search onSearch={this.onSearch} />
+        <div id="content-list">
+          <ArtistList />
+          <TrackList />
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
