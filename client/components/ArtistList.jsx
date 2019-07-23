f51365a2958441c5cac27577cf5b975aabb00e57
khengschew <khengc@gmail.com>
2019-07-22 19:23:26 -0700

    Stub out artist and track list components, styling

---
+++ client/components/ArtistList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ArtistList = (props) => (
+  <div className="list-view">
+    <h1>Artists</h1>
+  </div>
+);
+
+export default ArtistList;
---
+++ client/components/TrackList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TrackList = (props) => (
+  <div className="list-view">
+    <h1>Tracks</h1>
+  </div>
+);
+
+export default TrackList;
