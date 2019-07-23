4874ba34e61950f3e9346107ad17c997bf288549
khengschew <khengc@gmail.com>
2019-07-23 00:44:06 -0700

    Add pagination buttons to artist and track lists

--- client/components/ArtistList.jsx
+++ client/components/ArtistList.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Button } from "antd";
 import ArtistItem from "./ArtistItem";
 
 const ArtistList = ({ list }) => (
   <div className="list-view">
-    <h1>Artists</h1>
+    <div className="list-header">
+      <h1>Artists</h1>
+      <div className="list-header-buttons">
+        <Button type="link" icon="caret-left" />
+        <Button type="link" icon="caret-right" />
+      </div>
+    </div>
     {list.items.map((a, i) => (
       <ArtistItem item={a} key={a.id} />
     ))}
--- client/components/TrackList.jsx
+++ client/components/TrackList.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Button } from "antd";
 import TrackItem from "./TrackItem";
 
 const TrackList = ({ list }) => (
   <div className="list-view">
-    <h1>Tracks</h1>
+    <div className="list-header">
+      <h1>Tracks</h1>
+      <div className="list-header-buttons">
+        <Button type="link" icon="caret-left" />
+        <Button type="link" icon="caret-right" />
+      </div>
+    </div>
     {list.items.map((a, i) => (
       <TrackItem item={a} key={a.id} />
     ))}
